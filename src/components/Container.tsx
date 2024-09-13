69661522f7227569f7722fc396bce57c6db2491d
Thando <49730202+thandorsa@users.noreply.github.com>
2024-09-14 00:34:40 +0200

    Update Container.tsx

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -120,11 +120,11 @@ export default function Container(props: ContainerProps) {
         <meta content={meta.description} name="description" />
         <meta
           property="og:url"
-          content={`https://www.wendoj.codes${router.asPath}`}
+          content={`https://www.thandographx.xyz/${router.asPath}`}
         />
         <link
           rel="canonical"
-          href={`https://www.wendoj.codes${router.asPath}`}
+          href={`https://www.thandographx.xyz/${router.asPath}`}
         />
         <meta property="og:type" content={meta.type} />
         <meta property="og:site_name" content="Thando" />
